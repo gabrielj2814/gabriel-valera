8d738b8090478b13c6785563553868389a16b83a
gabriel <gabrielj2814@hotmail.com>
2022-08-22 21:46:35 -0400

    agregando layout base de la pagina esta quedando piola el sitio

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// const vistas={
-//     "inicio":true,
-//     "menu":false,
-//     "conociminetos":false,
-//     "proyecto":false,
-//     "detalles":false,
-//     "redes":false,
-//     "contactame":false,
-// }
