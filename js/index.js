0553a8ad97ffe342e0bdf5fbfd82337af8a48309
gabriel <gabrielj2814@hotmail.com>
2022-08-31 20:12:59 -0400

    agregando tracking al hacer scrolling Y se ilumine los enlaces del porfolio web en modo desktop

--- js/index.js
+++ js/index.js
@@ -5,6 +5,14 @@ let $botonUp=document.getElementById("botonUp")
 let trackingScroll= false
 
 let posicionesScrollY ={
+    // secciones del sistio web
+    seccionInicio: 0,
+    seccionSobreMi: 659,
+    seccionQueHago: 1192,
+    seccionResume: 1835,
+    seccionPorfolio: 3458,
+    seccionContacto: 4305,
+    // 
     mostrarBotonIrArriba: 684
 }
 
@@ -36,8 +44,49 @@ function irAlTopeDelaPagina(){
     window.scroll({top: 0,behavior: 'smooth'});
 }
 
-function ejecutarEventosSegunLaPosicionDelScroll(scroll_posicion){
-    // console.log("evento scroll ",scroll_posicion) .boton-up
+function activarEnlaceMenuDesktop(scroll_posicion){
+    let $barritaItemMenu=document.querySelector(".transition-barrita-item-menu")
+    if($barritaItemMenu!==null){
+        $barritaItemMenu.classList.remove("transition-barrita-item-menu")
+    }
+    if(scroll_posicion>=posicionesScrollY.seccionInicio && scroll_posicion<=posicionesScrollY.seccionSobreMi){
+        let $barrita =obtenerElementoHtmlDeUnElementoSeleccionado("enlaceDesktopInicio","barrita-item-menu")
+        $barrita.classList.add("transition-barrita-item-menu")
+    }
+    if(scroll_posicion>=posicionesScrollY.seccionSobreMi && scroll_posicion<=posicionesScrollY.seccionQueHago){
+        let $barrita =obtenerElementoHtmlDeUnElementoSeleccionado("enlaceDesktopSobreMi","barrita-item-menu")
+        $barrita.classList.add("transition-barrita-item-menu")
+    }
+    if(scroll_posicion>=posicionesScrollY.seccionQueHago && scroll_posicion<=posicionesScrollY.seccionResume){
+        let $barrita =obtenerElementoHtmlDeUnElementoSeleccionado("enlaceDesktopQueHago","barrita-item-menu")
+        $barrita.classList.add("transition-barrita-item-menu")
+    }
+    if(scroll_posicion>=posicionesScrollY.seccionResume && scroll_posicion<=posicionesScrollY.seccionPorfolio){
+        let $barrita =obtenerElementoHtmlDeUnElementoSeleccionado("enlaceDesktopResume","barrita-item-menu")
+        $barrita.classList.add("transition-barrita-item-menu")
+    }
+    if(scroll_posicion>=posicionesScrollY.seccionPorfolio && scroll_posicion<=posicionesScrollY.seccionContacto){
+        let $barrita =obtenerElementoHtmlDeUnElementoSeleccionado("enlaceDesktopPorfolio","barrita-item-menu")
+        $barrita.classList.add("transition-barrita-item-menu")
+    }
+    if(scroll_posicion>=posicionesScrollY.seccionContacto){
+        let $barrita =obtenerElementoHtmlDeUnElementoSeleccionado("enlaceDesktopContacto","barrita-item-menu")
+        $barrita.classList.add("transition-barrita-item-menu")
+    }
+}
+
+function obtenerElementoHtmlDeUnElementoSeleccionado(id,clase){
+    let $enlaceSeccionMenuDesktop=document.getElementById(id)
+    let arrayHtml=Object.entries($enlaceSeccionMenuDesktop.children).map(datos => datos[1])
+    let barrita = arrayHtml.find(htmlElement => {
+        if(htmlElement.classList.contains(clase)){
+            return htmlElement
+        }
+    })
+    return barrita
+}
+
+function activarBotonUp(scroll_posicion){
     if(scroll_posicion>=posicionesScrollY.mostrarBotonIrArriba){
         console.log("mostrar boton ir arriba")
         let $botonUp=document.querySelector(".boton-up")
@@ -62,12 +111,14 @@ function trackingScrollY(e){
     if(!trackingScroll){
         // * 
         window.requestAnimationFrame(function () {
-            ejecutarEventosSegunLaPosicionDelScroll(posicion)
+            activarBotonUp(posicion)
+            activarEnlaceMenuDesktop(posicion)
             trackingScroll= false
         })
     }
     trackingScroll=true
 }
+
 
 function quitarLoaderPage(){
     let $contenedorLoader=document.getElementById("contenedorLoader")
